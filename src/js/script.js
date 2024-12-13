e90bb533562c5e0ad2ca5fbe6bea1809c6a1105b
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2024-12-13 16:23:58 +0100

    Lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -21,10 +21,32 @@ const nextButton = document.querySelector('.carousel__button--next');
 const prevButton = document.querySelector('.carousel__button--prev');
 const carouselContainer = document.querySelector('.carousel__container');
 
+if (nextButton){
 nextButton.addEventListener('click', () => {
   carouselContainer.scrollBy({ left: 300, behavior: 'smooth' });
-});
-
+});}
+if (prevButton) {
 prevButton.addEventListener('click', () => {
   carouselContainer.scrollBy({ left: -300, behavior: 'smooth' });
+});}
+
+// *** LIGHTBOX ***
+const lightbox = document.querySelector('#lightbox');
+const listThumbnail = document.querySelectorAll("[data-full-img]");
+const lightboxImg = document.querySelector('#lightbox > img');
+
+listThumbnail.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', (evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.classList.add('open');
+        lightbox.classList.remove('close');
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.add('close');
+    lightbox.classList.remove('open');
+    setTimeout(() => {
+        lightbox.close();
+    }, 700); 
 });
